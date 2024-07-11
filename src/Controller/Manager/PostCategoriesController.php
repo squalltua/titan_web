649390540fb5052ed59660b8927876ae2856ec13
Pukkapol Tantikittipisut <squalltua@gmail.com>
2024-07-11 10:59:20 +0700

    implement post categories controller

--- src/Controller/Manager/PostCategoriesController.php
+++ src/Controller/Manager/PostCategoriesController.php
@@ -4,13 +4,13 @@ namespace App\Controller\Manager;
 
 use App\Controller\Manager\AppController;
 
-class PostTagsController extends AppController
+class PostCategoriesController extends AppController
 {
     public function initialize(): void
     {
         parent::initialize();
 
-        $this->set('menuActive', 'post-tags');
+        $this->set('menuActive', 'post-categories');
     }
 
     /**
@@ -29,14 +29,14 @@ class PostTagsController extends AppController
         $tag = $this->fetchTable('PostGroup')->newEmptyEntity();
         if ($this->request->is('post')) {
             $tag = $this->fetchTable('PostGroup')->patchEntity($tag, $this->request->getData());
-            $tag->type = 'tags';
+            $tag->type = 'categories';
             if ($this->fetchTable('PostGroup')->save($tag)) {
-                $this->Flash->success(__('The tag has been saved.'));
+                $this->Flash->success(__('The category has been saved.'));
 
-                return $this->redirect('/manager/posts/tags');
+                return $this->redirect('/manager/categories/tags');
             }
 
-            $this->Flash->error(__('The tag could not be saved. Please, try again.'));
+            $this->Flash->error(__('The category could not be saved. Please, try again.'));
         }
 
         $this->set(compact('tag'));
@@ -54,12 +54,12 @@ class PostTagsController extends AppController
         if ($this->request->is(['post', 'put', 'patch'])) {
             $tag = $this->fetchTable('PostGroup')->patchEntity($tag, $this->request->getData());
             if ($this->fetchTable('PostGroup')->save($tag)) {
-                $this->Flash->success(__('The tag has been saved.'));
+                $this->Flash->success(__('The category has been saved.'));
 
-                return $this->redirect('/manager/posts/tags');
+                return $this->redirect('/manager/categories/tags');
             }
 
-            $this->Flash->error(__('The tag could not be saved. Please, try again.'));
+            $this->Flash->error(__('The category could not be saved. Please, try again.'));
         }
 
         $this->set(compact('tag'));
@@ -76,11 +76,11 @@ class PostTagsController extends AppController
         $this->request->allowMethod(['post', 'delete']);
         $tag = $this->fetchTable('PostGroup')->get($id);
         if ($this->fetchTable('PostGroup')->delete($tag)) {
-            $this->Flash->success(__('The tag has been deleted.'));
+            $this->Flash->success(__('The category has been deleted.'));
             return $this->redirect('/manager/post-tags');
         }
 
-        $this->Flash->error(__('The tag could not be deleted. Please, try again.'));
-        return $this->redirect("/manager/posts/tags/edit/{$id}");
+        $this->Flash->error(__('The category could not be deleted. Please, try again.'));
+        return $this->redirect("/manager/posts/categories/edit/{$id}");
     }
 }
