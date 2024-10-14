0327a0814b83e674a3f3c90285e1d355e179fc5a
Pukkapol Tantikittipisut <squalltua@gmail.com>
2024-10-14 19:36:19 +0700

    implement product family

--- templates/Manager/Pim/ProductFamilies/edit.php
+++ templates/Manager/Pim/ProductFamilies/edit.php
@@ -1,27 +1,60 @@
-<div class="card shadow-sm">
-    <div class="card-body">
-        <div class="d-flex mb-2">
-            <div class="flex-grow-1">
-                <h4><?= __('Product Families :: Edit') ?></h4>
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <div class="page-pretitle">
+                    <?= __('Edit information') ?>
+                </div>
+                <h2 class="page-title">
+                    <?= __('Product Families') ?>
+                </h2>
+            </div>
+            <div class="col-auto ms-auto">
+                <a href="/manager/pim/product-families/add" class="btn btn-primary d-none d-sm-inline-block">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                         stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <line x1="12" y1="5" x2="12" y2="19"/>
+                        <line x1="5" y1="12" x2="19" y2="12"/>
+                    </svg>
+                    <?= __('Create new') ?>
+                </a>
             </div>
         </div>
+    </div>
+</div>
+
+<div class="page-body">
+    <div class="container-xl">
         <?= $this->Form->create($family) ?>
         <div class="row">
             <div class="col-6">
-                <div class="form-group mb-3">
-                    <label for="name" class="form-label"><?= __('Family name') ?></label>
-                    <?= $this->Form->text('name', ['class' => 'form-control', 'id' => 'name']) ?>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"><?= __('Product family information') ?></h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3 row">
+                            <label for="name" class="col-3 col-form-label required"><?= __('Family name') ?></label>
+                            <div class="col">
+                                <?= $this->Form->text('name', ['class' => 'form-control', 'id' => 'name', 'required' => true]) ?>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="description" class="col-3 col-form-label"><?= __('Descrition') ?></label>
+                            <div class="col">
+                                <?= $this->Form->textarea('description', ['class' => 'form-control', 'id' => 'description']) ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-end">
+                        <div class="d-flex">
+                            <?= $this->Html->link(__('Cancel'), '/manager/pim/product-families', ['class' => 'btn btn-link']) ?>
+                            <?= $this->Form->button(__('Save'), ['class' => 'btn btn-primary ms-auto']) ?>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group mb-3">
-                    <label for="description" class="form-label"><?= __('Description') ?></label>
-                    <?= $this->Form->textarea('description', ['class' => 'form-control', 'id' => 'description']) ?>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <?= $this->Form->button(__('Save'), ['class' => 'btn btn-primary']) ?>
-                <?= $this->Html->link(__('Back'), '/manager/pim/product-families', ['class' => 'btn btn-secondary']) ?>
             </div>
         </div>
         <?= $this->Form->end() ?>
--- templates/Manager/Pim/Products/add.php
+++ templates/Manager/Pim/Products/add.php
@@ -12,11 +12,11 @@
             <div class="col-auto ms-auto">
                 <a href="/manager/pim/products/add" class="btn btn-primary d-none d-sm-inline-block">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <line x1="12" y1="5" x2="12" y2="19" />
-                        <line x1="5" y1="12" x2="19" y2="12" />
+                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                         stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <line x1="12" y1="5" x2="12" y2="19"/>
+                        <line x1="5" y1="12" x2="19" y2="12"/>
                     </svg>
                     <?= __('Create new') ?>
                 </a>
@@ -28,53 +28,55 @@
 <div class="page-body">
     <div class="container-xl">
         <?= $this->Form->create($product) ?>
-        <div class="col-6">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title"><?= __('Product information') ?></h3>
-                </div>
-                <div class="card-body">
-                    <div class="mb-3 row">
-                        <label for="title" class="col-3 col-form-label required"><?= __('Product name') ?></label>
-                        <div class="col">
-                            <?= $this->Form->text('title', ['class' => 'form-control', 'id' => 'title', 'required' => true]) ?>
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"><?= __('Product information') ?></h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3 row">
+                            <label for="title" class="col-3 col-form-label required"><?= __('Product name') ?></label>
+                            <div class="col">
+                                <?= $this->Form->text('title', ['class' => 'form-control', 'id' => 'title', 'required' => true]) ?>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label form="summary" class="col-3 col-form-label"><?= __('Summary') ?></label>
+                            <div class="col">
+                                <?= $this->Form->textarea('summary', ['class' => 'form-control', 'id' => 'summary']) ?>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="content" class="col-3 col-form-label required"><?= __('Content') ?></label>
+                            <div class="col">
+                                <?= $this->Form->textarea('content', ['class' => 'form-control', 'id' => 'content', 'required' => true]) ?>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="sku" class="col-3 col-form-label required"><?= __('SKUs') ?></label>
+                            <div class="col">
+                                <?= $this->Form->text('sku', ['class' => 'form-control', 'id' => 'sku', 'required' => true]) ?>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="model-name" class="col-3 col-form-label"><?= __('Model name') ?></label>
+                            <div class="col">
+                                <?= $this->Form->text('model_name', ['class' => 'form-control', 'id' => 'model-name']) ?>
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <label for="series-number" class="col-3 col-form-label"><?= __('Series number') ?></label>
+                            <div class="col">
+                                <?= $this->Form->text('series_number', ['class' => 'form-control', 'id' => 'series-number']) ?>
+                            </div>
                         </div>
                     </div>
-                    <div class="mb-3 row">
-                        <label form="summary" class="col-3 col-form-label"><?= __('Summary') ?></label>
-                        <div class="col">
-                            <?= $this->Form->textarea('summary', ['class' => 'form-control', 'id' => 'summary']) ?>
+                    <div class="card-footer text-end">
+                        <div class="d-flex">
+                            <?= $this->Html->link(__('Cancel'), '/manager/pim/products', ['class' => 'btn btn-link']) ?>
+                            <?= $this->Form->button(__('Save'), ['class' => 'btn btn-primary ms-auto']) ?>
                         </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="content" class="col-3 col-form-label required"><?= __('Content') ?></label>
-                        <div class="col">
-                            <?= $this->Form->textarea('content', ['class' => 'form-control', 'id' => 'content', 'required' => true]) ?>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="sku" class="col-3 col-form-label required"><?= __('SKUs') ?></label>
-                        <div class="col">
-                            <?= $this->Form->text('sku', ['class' => 'form-control', 'id' => 'sku', 'required' => true]) ?>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="model-name" class="col-3 col-form-label"><?= __('Model name') ?></label>
-                        <div class="col">
-                            <?= $this->Form->text('model_name', ['class' => 'form-control', 'id' => 'model-name']) ?>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <label for="series-number" class="col-3 col-form-label"><?= __('Series number') ?></label>
-                        <div class="col">
-                            <?= $this->Form->text('series_number', ['class' => 'form-control', 'id' => 'series-number']) ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer text-end">
-                    <div class="d-flex">
-                        <?= $this->Html->link(__('Cancel'), '/manager/pim/products', ['class' => 'btn btn-link']) ?>
-                        <?= $this->Form->button(__('Save'), ['class' => 'btn btn-primary ms-auto']) ?>
                     </div>
                 </div>
             </div>
