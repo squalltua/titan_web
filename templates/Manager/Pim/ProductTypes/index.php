5cc125940d2ec25b0989ca49fd3f14408f9ff789
Pukkapol Tantikittipisut <squalltua@gmail.com>
2024-10-14 22:56:16 +0700

    implement Product type views

--- templates/Manager/Pim/ProductTypes/index.php
+++ templates/Manager/Pim/ProductTypes/index.php
@@ -12,11 +12,11 @@
             <div class="col-auto ms-auto">
                 <a href="/manager/pim/product-types/add" class="btn btn-primary d-none d-sm-inline-block">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <line x1="12" y1="5" x2="12" y2="19" />
-                        <line x1="5" y1="12" x2="19" y2="12" />
+                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                         stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <line x1="12" y1="5" x2="12" y2="19"/>
+                        <line x1="5" y1="12" x2="19" y2="12"/>
                     </svg>
                     <?= __('Create new') ?>
                 </a>
@@ -27,62 +27,62 @@
 
 <div class="page-body">
     <?php if ($types->count()): ?>
-    <div class="container-xl">
-        <div class="card">
-            <div class="table-responsive">
-                <table class="table table-vcenter card-table table-nowrap datatable">
-                    <thead>
+        <div class="container-xl">
+            <div class="card">
+                <div class="table-responsive">
+                    <table class="table table-vcenter card-table table-nowrap datatable">
+                        <thead>
                         <tr>
                             <th><?= $this->Paginator->sort('name') ?></th>
                             <th class="w-1"></th>
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
                         <?php foreach ($types as $type): ?>
-                        <tr>
-                            <td><?= h($type->name) ?></td>
-                            <td class="text-end">
-                                <?= $this->Html->link(__('Edit'), "/manager/pim/product-types/edit/{$type->id}", ['class' => 'me-3']) ?>
-                                <?= $this->Form->postLink(__('Delete'), "/manager/pim/product-types/delete/{$type->id}", ['class' => 'text-danger', 'confirm' => __('Do you want delete this data?')]) ?>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td><?= h($type->name) ?></td>
+                                <td class="text-end">
+                                    <?= $this->Html->link(__('Edit'), "/manager/pim/product-types/edit/{$type->id}", ['class' => 'me-3']) ?>
+                                    <?= $this->Form->postLink(__('Delete'), "/manager/pim/product-types/delete/{$type->id}", ['class' => 'text-danger', 'confirm' => __('Do you want delete this data?')]) ?>
+                                </td>
+                            </tr>
                         <?php endforeach ?>
-                    </tbody>
-                </table>
-            </div>
-            <div class="card-footer d-flex align-items-center">
-                <ul class="pagination m-0">
-                    <?= $this->Paginator->prev('« Previous') ?>
-                    <?= $this->Paginator->numbers() ?>
-                    <?= $this->Paginator->next('Next »') ?>
-                </ul>
-                <p class="m-0 ms-auto text-secondary"><?= $this->Paginator->counter('range'); ?></p>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer d-flex align-items-center">
+                    <ul class="pagination m-0">
+                        <?= $this->Paginator->prev('« Previous') ?>
+                        <?= $this->Paginator->numbers() ?>
+                        <?= $this->Paginator->next('Next »') ?>
+                    </ul>
+                    <p class="m-0 ms-auto text-secondary"><?= $this->Paginator->counter('range'); ?></p>
+                </div>
             </div>
         </div>
-    </div>
     <?php else: ?>
-    <div class="container-xl d-flex flex-column justify-content-center">
-        <div class="empty">
-            <div class="empty-img">
-                <?= $this->Html->image('undraw_blank_canvas_re_2hwy.svg', ['height' => 128]) ?>
-            </div>
-            <p class="empty-title"><?= __('No results found') ?></p>
-            <p class="empty-subtitle text-secondary">
-                <?= __('Try adjusting your search or filter to find what you\'re looking for.') ?>
-            </p>
-            <div class="empty-action">
-                <a href="/manager/pim/product-types/add" class="btn btn-primary">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                        stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M12 5l0 14" />
-                        <path d="M5 12l14 0" />
-                    </svg>
-                    <?= __('Add your first product type') ?>
-                </a>
+        <div class="container-xl d-flex flex-column justify-content-center">
+            <div class="empty">
+                <div class="empty-img">
+                    <?= $this->Html->image('undraw_blank_canvas_re_2hwy.svg', ['height' => 128]) ?>
+                </div>
+                <p class="empty-title"><?= __('No results found') ?></p>
+                <p class="empty-subtitle text-secondary">
+                    <?= __('Try adjusting your search or filter to find what you\'re looking for.') ?>
+                </p>
+                <div class="empty-action">
+                    <a href="/manager/pim/product-types/add" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                             stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                             stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M12 5l0 14"/>
+                            <path d="M5 12l14 0"/>
+                        </svg>
+                        <?= __('Add your first product type') ?>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
     <?php endif ?>
 </div>
