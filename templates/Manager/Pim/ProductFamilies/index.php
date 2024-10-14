0c0555077205de4287a708036da3077e991cf75f
Pukkapol Tantikittipisut <squalltua@gmail.com>
2024-10-14 22:43:17 +0700

    reformat code

--- templates/Manager/Pim/ProductFamilies/index.php
+++ templates/Manager/Pim/ProductFamilies/index.php
@@ -11,13 +11,15 @@
             </div>
             <div class="col-auto ms-auto">
                 <a href="/manager/pim/product-families/add" class="btn btn-primary d-none d-sm-inline-block">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <line x1="12" y1="5" x2="12" y2="19" />
-                      <line x1="5" y1="12" x2="19" y2="12" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                         stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <line x1="12" y1="5" x2="12" y2="19"/>
+                        <line x1="5" y1="12" x2="19" y2="12"/>
                     </svg>
                     <?= __('Create new') ?>
-                  </a>
+                </a>
             </div>
         </div>
     </div>
@@ -29,14 +31,14 @@
             <div class="table-responsive">
                 <table class="table table-vcenter card-table table-nowrap datatable">
                     <thead>
-                        <tr>
-                            <th><?= $this->Paginator->sort('name') ?></th>
-                            <th><?= $this->Paginator->sort('description') ?></th>
-                            <th class="w-1"></th>
-                        </tr>
+                    <tr>
+                        <th><?= $this->Paginator->sort('name') ?></th>
+                        <th><?= $this->Paginator->sort('description') ?></th>
+                        <th class="w-1"></th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <?php if ($families->count()): ?>
+                    <?php if ($families->count()): ?>
                         <?php foreach ($families as $family): ?>
                             <tr>
                                 <td><?= h($family->name) ?></td>
@@ -47,11 +49,11 @@
                                 </td>
                             </tr>
                         <?php endforeach ?>
-                        <?php else: ?>
-                            <tr>
-                                <td colspan="3" class="text-center"><?= __('No data') ?></td>
-                            </tr>
-                        <?php endif ?>
+                    <?php else: ?>
+                        <tr>
+                            <td colspan="3" class="text-center"><?= __('No data') ?></td>
+                        </tr>
+                    <?php endif ?>
                     </tbody>
                 </table>
             </div>
