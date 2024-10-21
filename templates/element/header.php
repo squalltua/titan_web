afc5e9ff4ec38ccdd5e85eabdc26e24f61b994e5
Pukkapol Tantikittipisut <squalltua@gmail.com>
2024-10-21 23:18:38 +0700

    fixed menu

--- templates/element/header.php
+++ templates/element/header.php
@@ -1,7 +1,7 @@
 <header class="navbar navbar-expand-md sticky-top d-print-none">
     <div class="container-xl">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu"
-            aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
@@ -12,7 +12,7 @@
         <div class="navbar-nav flex-row order-md-last">
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
-                    aria-label="Open user menu">
+                   aria-label="Open user menu">
                     <span class="avatar avatar-sm" style="background-image: url()"></span>
                     <div class="d-none d-xl-block ps-2">
                         <div><?= $this->Identity->get('full_name'); ?></div>
@@ -20,9 +20,7 @@
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <a href="/manager/settings/users/profile" class="dropdown-item">Profile</a>
-                    <div class="dropdown-divider"></div>
-                    <a href="/manager/logout" class="dropdown-item">Logout</a>
+                    <a href="/manager/logout" class="dropdown-item"><?= __('Logout') ?></a>
                 </div>
             </div>
         </div>
@@ -31,18 +29,18 @@
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#navbar-third" data-bs-toggle="dropdown"
-                            data-bs-auto-close="outside" role="button" aria-expanded="false">
+                           data-bs-auto-close="outside" role="button" aria-expanded="false">
                             <span
                                 class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/star -->
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    class="icon icon-tabler icons-tabler-outline icon-tabler-baseline-density-small">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M4 3h16" />
-                                    <path d="M4 9h16" />
-                                    <path d="M4 15h16" />
-                                    <path d="M4 21h16" />
+                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                     stroke-linejoin="round"
+                                     class="icon icon-tabler icons-tabler-outline icon-tabler-baseline-density-small">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M4 3h16"/>
+                                    <path d="M4 9h16"/>
+                                    <path d="M4 15h16"/>
+                                    <path d="M4 21h16"/>
                                 </svg>
                             </span>
                             <span class="nav-link-title">
@@ -50,14 +48,21 @@
                             </span>
                         </a>
                         <div class="dropdown-menu">
-                            <a class="dropdown-item" href="/manager"><?= __('All') ?></a>
-                            <a class="dropdown-item" href="/manager/wcm/posts"><?= __('Web Content management') ?></a>
-                            <a class="dropdown-item"
-                                href="/manager/pim/products"><?= __('Product information management') ?></a>
-                            <a class="dropdown-item"
-                                href="/manager/dam/medias"><?= __('Digital assets management') ?></a>
-                            <a class="dropdown-item"
-                                href="/manager/cdm/customers"><?= __('Customer data manangment') ?></a>
+                            <a class="dropdown-item" href="/manager">
+                                <?= __('All') ?>
+                            </a>
+                            <a class="dropdown-item" href="/manager/wcm/posts">
+                                <?= __('Web Content management') ?>
+                            </a>
+                            <a class="dropdown-item" href="/manager/pim/products">
+                                <?= __('Product information management') ?>
+                            </a>
+                            <a class="dropdown-item" href="/manager/dam/medias">
+                                <?= __('Digital assets management') ?>
+                            </a>
+                            <a class="dropdown-item" href="/manager/cdm/customers">
+                                <?= __('Customer data manangment') ?>
+                            </a>
                         </div>
                     </li>
                 </ul>
--- templates/element/top_menu.php
+++ templates/element/top_menu.php
@@ -1,38 +1,63 @@
 <nav class="navbar navbar-expand-lg bg-white">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/office">TitanWEB</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <a class="navbar-brand" href="/manager">TitanWEB</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <?= $applicationName ?? __('Application') ?>
-            </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="/manager"><?= __('All') ?></a></li>
-                <li><a class="dropdown-item" href="/manager/wcm/posts"><?= __('Web Content management') ?></a></li>
-                <li><a class="dropdown-item" href="/manager/pim/products"><?= __('Product information management') ?></a></li>
-                <li><a class="dropdown-item" href="/manager/dam/medias"><?= __('Digital assets management') ?></a></li>
-                <li><a class="dropdown-item" href="/manager/cdm/customers"><?= __('Customer data manangment') ?></a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="/manager/settings"><?= __('Settings management') ?></a></li>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                       aria-expanded="false">
+                        <?= $applicationName ?? __('Application') ?>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="dropdown-item" href="/manager"><?= __('All') ?></a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/manager/wcm/posts">
+                                <?= __('Web Content management') ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/manager/pim/products">
+                                <?= __('Product information management') ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/manager/dam/medias">
+                                <?= __('Digital assets management') ?>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/manager/cdm/customers">
+                                <?= __('Customer data manangment') ?>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="/manager/settings">
+                                <?= __('Settings management') ?>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
-            </li>
-        </ul>
-        <ul class="navbar-nav mb-2 mb-lg-0">
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <?= $this->Identity->get('username'); ?>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-                <li><a class="dropdown-item" href="/manager/settings/users/profile"><?= __('Profile') ?></a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="/manager/logout"><?= __('Logout') ?></a></li>
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                       aria-expanded="false">
+                        <?= $this->Identity->get('username'); ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="/manager/logout"><?= __('Logout') ?></a></li>
+                    </ul>
+                </li>
             </ul>
-            </li>
-        </ul>
         </div>
     </div>
 </nav>
