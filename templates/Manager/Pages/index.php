99b7233c327152f1d354c5e574dcf2e85d12964e
Pukkapol Tantikittipisut <squalltua@gmail.com>
2024-10-22 13:38:53 +0700

    fixed starter page view

--- templates/Manager/Pages/index.php
+++ templates/Manager/Pages/index.php
@@ -1,1 +1,17 @@
-<h1>Start page</h1>
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <h2 class="page-title">
+                    Starter page
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="page-body">
+    <div class="container-xl">
+        <h1>Empty</h1>
+    </div>
+</div>
