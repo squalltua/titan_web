3fbc6a81a72e358bd9b8db1efb6e8ad3fbd8ef30
Pukkapol Tantikittipisut <squalltua@gmail.com>
2025-01-17 12:02:33 +0700

    change footer text

--- templates/layout/manager.php
+++ templates/layout/manager.php
@@ -53,10 +53,8 @@
                         <div class="col-12 col-lg-auto mt-3 mt-lg-0">
                             <ul class="list-inline list-inline-dots mb-0">
                                 <li class="list-inline-item">
-                                    Copyright &copy; <?= date('Y') ?>
-                                    <a href="https://www.titanscript.com" class="link-secondary">Titanscript business
-                                        consult col.,ltd</a>.
-                                    All rights reserved.
+                                    &copy; <?= date('Y') ?>
+                                    Titan/WEB open source. MIT LC.
                                 </li>
                                 <li class="list-inline-item">
                                     Version 1.0.0 (beta)
