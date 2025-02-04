42936f4a70957fae6e4b8f34e289597c55e92caf
Pukkapol Tantikittipisut <squalltua@gmail.com>
2025-02-04 15:30:05 +0700

    add database for install and removed font style

--- templates/layout/login.php
+++ templates/layout/login.php
@@ -13,17 +13,6 @@
         '/vendor/tabler-v1.0.0-beta20/dist/css/tabler-vendors.min.css',
         'main.css',
     ]) ?>
-    <style>
-        @import url('https://rsms.me/inter/inter.css');
-
-        :root {
-            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
-        }
-
-        body {
-            font-feature-settings: "cv03", "cv04", "cv11";
-        }
-    </style>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
