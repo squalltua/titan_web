c03229d943ce388d1ef607fcb548ac1919915ddd
Pukkapol Tantikittipisut <squalltua@gmail.com>
2025-07-27 12:48:55 +0700

    update post button

--- templates/Manager/Wcm/Posts/view.php
+++ templates/Manager/Wcm/Posts/view.php
@@ -10,15 +10,14 @@
                 </h2>
             </div>
             <div class="col-auto ms-auto">
-                <a href="/manager/wcm/posts/add" class="btn btn-primary d-none d-sm-inline-block">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                        stroke-linejoin="round">
+                <a href="/manager/wcm/posts" class="btn btn-secondary d-none d-sm-inline-block">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-left-dashed">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <line x1="12" y1="5" x2="12" y2="19" />
-                        <line x1="5" y1="12" x2="19" y2="12" />
+                        <path d="M5 12h6m3 0h1.5m3 0h.5" />
+                        <path d="M5 12l4 4" />
+                        <path d="M5 12l4 -4" />
                     </svg>
-                    <?= __('Create new') ?>
+                    <?= __('Back to list') ?>
                 </a>
             </div>
         </div>
